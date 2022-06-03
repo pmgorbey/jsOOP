36c27dcbb6234f725132b9e182dbf77989725993
pmgorbey <pmgorbey@gmail.com>
2022-06-03 16:59:04 +0300

    commit 3

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Hello Petro');
+class Employee {
+	constructor(name, position, department) {
+		this.name = name;
+		this.position = position;
+		this.department = department;
+	}
+}
+
+class Name{
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Departament{
+    constructor(departament) {
+        this.departament = departament;
+    }
+    
+}
+
+
+let name = new Name('pet');
+let employee = new Employee(name, 'right', 'it');
+
+console.log(employee.name);
+console.log(employee.position);
+console.log(employee.department);
+
